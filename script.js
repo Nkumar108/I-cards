7b2538e50d7b1a3764e2c4ea29e25a8eb85e5c42
Nkumar108 <nkumnk67@gmail.com>
2025-07-06 21:21:17 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+document.getElementById("name").oninput = (e) => document.getElementById("card-name").innerText = e.target.value;
+document.getElementById("designation").oninput = (e) => document.getElementById("card-designation").innerText = e.target.value;
+document.getElementById("idNo").oninput = (e) => document.getElementById("card-idNo").innerText = e.target.value;
+document.getElementById("dob").oninput = (e) => document.getElementById("card-dob").innerText = e.target.value;
+document.getElementById("email").oninput = (e) => document.getElementById("card-email").innerText = e.target.value;
+document.getElementById("phone").oninput = (e) => document.getElementById("card-phone").innerText = e.target.value;
+document.getElementById("website").oninput = (e) => document.getElementById("card-website").innerText = e.target.value;
+
+document.getElementById("photo").onchange = (e) => {
+  const reader = new FileReader();
+  reader.onload = function(event) {
+    document.getElementById("preview-photo").src = event.target.result;
+  };
+  reader.readAsDataURL(e.target.files[0]);
+};
+
+function downloadCard() {
+  html2canvas(document.getElementById("card-front")).then(canvas => {
+    const link = document.createElement('a');
+    link.download = 'id-card-front.png';
+    link.href = canvas.toDataURL();
+    link.click();
+  });
+}
